57b9f5f728592a32da070aeee392d4df8259c819
Jakob Bornecrantz <wallbraker@gmail.com>
2012-05-13 23:53:15 +0200

    Change the binary depending on platform
    Signed-off-by: Jakob Bornecrantz <wallbraker@gmail.com>

--- ChargedMinersLauncher/SignInForm.cs
+++ ChargedMinersLauncher/SignInForm.cs
@@ -10,7 +10,13 @@ using System.IO;
 namespace ChargedMinersLauncher {
     public sealed partial class SignInForm : Form {
         ServerInfo[] servers;
-        public const string ChargeBinary = "Charge.exe";
+        public static string ChargeBinary;
+        public static string ChargeBinaryAlt;
+        public const string ChargeBinaryFormatWindows = "Charge.{0}.exe";
+        public const string ChargeBinaryFormatMacOSX = "Charge.{0}.MacOSX";
+        public const string ChargeBinaryFormatLinux = "Charge.{0}.Linux";
+        public const string ChargeBinaryFormat32Bit = "i386";
+        public const string ChargeBinaryFormat64Bit = "x86_64";
         const string PasswordSaveFile = "saved-login.dat";
 
         static readonly Regex
@@ -23,9 +29,37 @@ namespace ChargedMinersLauncher {
 
 
         public SignInForm() {
-            if( !File.Exists( ChargeBinary ) ) {
-                WarningForm.Show( "Warning", "Charge.exe not found!" );
+
+            string tmp;
+            if( RuntimeInfo.IsWindows ) {
+                tmp = ChargeBinaryFormatWindows;
+            } else if( RuntimeInfo.IsMacOSX ) {
+                tmp = ChargeBinaryFormatMacOSX;
+            } else if( RuntimeInfo.IsLinux ) {
+                tmp = ChargeBinaryFormatLinux;
+            } else {
+                // XXX Something better here.
+                tmp = null;
             }
+
+            if( RuntimeInfo.Is32Bit ) {
+                ChargeBinary = String.Format( tmp, ChargeBinaryFormat32Bit );
+                ChargeBinaryAlt = "";
+            } else if ( RuntimeInfo.Is64Bit ) {
+                ChargeBinary = String.Format( tmp, ChargeBinaryFormat64Bit );
+                ChargeBinaryAlt = String.Format( tmp, ChargeBinaryFormat32Bit );
+            }
+
+            // XXX This code should be moved into the auto updater.
+            if( !File.Exists( ChargeBinary ) ) {
+                // Look for both.
+                if ( !File.Exists( ChargeBinary ) ) {
+                    WarningForm.Show( "Warning", String.Format( "The binray \"{0}\" not found!", ChargeBinary ) );
+                } else {
+                    ChargeBinary = ChargeBinaryAlt;
+                }
+            }
+
             InitializeComponent();
             string passwordFileFullName = Path.Combine( ChargedMinersSettings.ConfigPath, PasswordSaveFile );
             try {
