73d79a2ad789abab5025174967e2753d9a788bec
Jakob Bornecrantz <wallbraker@gmail.com>
2012-05-14 03:42:21 +0200

    Add classes for parsing a version file
    Signed-off-by: Jakob Bornecrantz <wallbraker@gmail.com>

---
+++ ChargedMinersLauncher/VersionInfo.cs
@@ -0,0 +1,30 @@
+// Part of ChargedMinersLaunher | Copyright (c) 2012 Jakob Bornecrantz <wallbraker@gmail.com> | BSD-3 | See LICENSE.txt
+using System;
+using System.Linq;
+
+namespace ChargedMinersLauncher {
+    public class VersionInfo {
+        public string Name { get; private set; }
+        public string HttpName { get; private set; }
+        public string Md5 { get; private set; }
+
+        public VersionInfo( string name, string httpName, string md5 ) {
+            Name = name;
+            HttpName = httpName;
+            Md5 = md5.ToLower();
+
+            if( Md5.Length != 32 ) {
+               throw new System.ArgumentException( "Not a valid md5 string array" );
+            }
+        }
+
+        public bool CompareHash( byte[] other ) {
+            var str = BitConverter.ToString(other).Replace("-", "").ToLower();
+            return String.Compare( Md5, str ) == 0;
+        }
+
+        public bool CompareHash( string other ) {
+            return String.Compare( Md5, other.ToLower() ) == 0;
+        }
+    }
+}
---
+++ ChargedMinersLauncher/VersionsTxt.cs
@@ -0,0 +1,49 @@
+﻿// Part of ChargedMinersLaunher | Copyright (c) 2012 Jakob Bornecrantz <wallbraker@gmail.com> | BSD-3 | See LICENSE.txt
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ChargedMinersLauncher {
+    sealed class VersionsTxt {
+        static readonly Regex CommentRegex = new Regex( @"^\s*#" );
+        static readonly Regex NameRegex = new Regex( @"^(\S+)\s*$" );
+        static readonly Regex Md5AndNameRegex = new Regex( @"\s*([a-fA-F0-9]{32})\s+(\S+)\s*$" );
+
+        private Dictionary<string, VersionInfo> versions = new Dictionary<string, VersionInfo>();
+
+        public VersionsTxt( IEnumerable<string> lines ) {
+
+            string name = "";
+            bool nameFound = false;
+            foreach( string line in lines ) {
+                if( line == null ) break;
+                if( line.Length == 0 || CommentRegex.IsMatch( line ) ) continue;
+
+                var match = NameRegex.Match( line );
+                if( match.Success ) {
+                    nameFound = true;
+                    name = match.Groups[1].Value;
+                    continue;
+                }
+
+                match = Md5AndNameRegex.Match( line );
+                if( match.Success && nameFound ) {
+                    nameFound = false;
+
+                    var md5 = match.Groups[1].Value;
+                    var httpName = match.Groups[2].Value;
+
+                    versions.Add( name, new VersionInfo( name, httpName, md5 ) );
+                }
+            }
+        }
+
+        public VersionInfo Get( string name ) {
+            VersionInfo ret = null;
+            versions.TryGetValue( name, out ret );
+            return ret;
+        }
+    }
+}
